5c34f559f6d6718b37ceed7cea28d9395d1e0f76
AnyaFL1CKINGER <98411479+flickin4@users.noreply.github.com>
2022-11-09 18:57:26 +0000

    Got toargv to work and the source changes properly

--- .ccls-cache/@home@runner@Lab-10/main.c
+++ .ccls-cache/@home@runner@Lab-10/main.c
@@ -1,47 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int toArgv(char *source, char *dest[], int size) {
   const char whitespace[7] = " \t\n\v\f\r";
-  dest = malloc(size);
-  printf("\n7");
-  fflush(stdout);
-  strtok(source, whitespace);
-  printf("\n9");
-  fflush(stdout);
-  char *token = "HI";
+  char *token;
+  token = strtok(source, whitespace);
   int currentIndex = 0;
   while (token != NULL && currentIndex < size - 1) {
-    printf("12");
-    fflush(stdout);
-    // int stringLength = strlen(token);
-    // dest[currentIndex] = malloc(stringLength);
-    // strncpy(dest[currentIndex], token, stringLength);
     dest[currentIndex] = token;
-    printf("16");
-    fflush(stdout);
+    printf("\n token: %s", token);
+    printf("\n currentIndex: %s", dest[currentIndex]);
     currentIndex++;
     token = strtok(NULL, " ");
-    printf("20");
-    fflush(stdout);
   }
-  printf("22");
-  fflush(stdout);
   dest[currentIndex] = NULL;
   return currentIndex;
 }
 
-int main(void) {
-  char *arr[6];
-  int retval = toArgv("my dog has fleas", arr, 6);
-  printf("\n%d", retval);
-  printf("\n%s", arr[0]);
-  printf("\n%s", arr[1]);
-  printf("\n%s", arr[2]);
-  printf("\n%s", arr[3]);
-  printf("\n%s", arr[4]);
-  printf("\n%s", arr[5]);
+int main(int argc, char *argv[]) {
+  for (int i = 1; i < argc; i++) {
+    char *tempArr[10];
+    int retval = toArgv(argv[i], tempArr, 10);
+    printf("\n%d", retval);
+    printf("\n%s", tempArr[0]);
+    printf("\n%s", tempArr[1]);
+    printf("\n%s", tempArr[2]);
+    printf("\n%s", tempArr[3]);
+    printf("\n%s", tempArr[4]);
+    printf("\n%s", tempArr[5]);
+
+		for(int i = 0; i < retval; i++) {			
+    	int id = fork();
+			if(id) {
+				wait();
+			} else {
+				execvp(tempArr[i]);
+			}
+		}
+  }
+  // for each command line argument in argv
+  //   Call toArgv() to break it into an array of tokens named tempArr.
+  //   (Assume there will be no more than 10 tokens.)
+  //   call fork
+  //   if in the parent
+  //      call wait
+  //   else (in the child)
+  //      call execvp with tempArr
 
   printf("Hello World\n");
   return 0;
